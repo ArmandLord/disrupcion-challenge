18adf9077b2fb837cca5d025f02b9e59cd6a29f4
Armando Perez <armandlord98@gmail.com>
2023-04-10 18:47:58 -0600

    imp: add sweetalert2

--- src/components/ContainerApp.tsx
+++ src/components/ContainerApp.tsx
@@ -2,9 +2,16 @@ import { PropsWithChildren } from "react";
 
 export const ContainerApp = ({ children }: PropsWithChildren) => {
   return (
-    <div className="container vh-100 bg-primary">
+    <div className="container vh-100 ">
       <div className="row justify-content-center align-items-center">
-        <div className="col-md-4 bg-secondary">{children}</div>
+        <div
+          style={{
+            position: "relative",
+          }}
+          className="col-md-4 bg-secondary"
+        >
+          {children}
+        </div>
       </div>
     </div>
   );
